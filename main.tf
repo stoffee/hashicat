4494eece4aecac467cc2f59f23f4571c1cbf7fde
stoffee <chris@roadkill.org>
2020-02-20 08:37:37 -0800

    changed https port

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_network_security_group" "catapp-sg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "443"
+    destination_port_range     = "444"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
