4c6d1e4f6f9e1596ec7a760ec6450f16162afe0e
stoffee <chris@roadkill.org>
2020-02-19 23:13:45 -0800

    updated cataas.com

--- variables.tf
+++ variables.tf
@@ -70,6 +70,6 @@ variable "width" {
 }
 
 variable "placeholder" {
-  default     = "placekitten.com"
+  default     = "cataas.com/cat"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
